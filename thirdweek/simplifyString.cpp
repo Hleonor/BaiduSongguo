7e96657795b5170a30ecb739af7360f43f33d107
moc <1287607885@qq.com>
2022-09-23 17:37:16 +0800

    主字符串应该为全局字符串，由主要的for循环进行遍历

--- thirdweek/simplifyString.cpp
+++ thirdweek/simplifyString.cpp
@@ -1,3 +1,66 @@
 //
 // Created by Hleonor on 2022-09-23.
 //
+#include <iostream>
+#include <string>
+using namespace std;
+struct node* createRoot(struct node* root);
+void handleString(struct node* node);
+struct node
+{
+    string node_str;
+    int len = 0;
+    struct node* parent = NULL;
+    struct node* left = NULL;
+    struct node* right = NULL;
+    node(){}; //构造函数
+};
+
+string mainString;
+int main()
+{
+    struct node* root = createRoot(root);
+    cin >> mainString;
+    handleString(root);
+    printf("%d" , root->len);
+    return 0;
+}
+
+node* createRoot(node* root)
+{
+    root = new node();
+    return root;
+}
+
+node* createNode(string sub_str , node* father)
+{
+    node* new_node = new node();
+    new_node->node_str = sub_str;
+    new_node->len = sub_str.length();
+    new_node->parent = father;
+    return new_node;
+}
+
+void handleString(node* node)
+{
+    string temp; // 临时存储字符串
+    for (int i = 0; i < mainString.size(); i++)
+    {
+        if (mainString[i] == 'a')
+        {
+            temp += mainString[i];
+        }
+        else if (mainString[i] == '|') // 遇到
+        {
+
+        }
+        else if (mainString[i] == '(')
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
