839d02244126851903db5faa2f0453837ba5fd8d
moc <1287607885@qq.com>
2022-10-01 21:16:45 +0800

    第一题超时做法

--- FourthWeek/Question1.cpp
+++ FourthWeek/Question1.cpp
@@ -17,9 +17,11 @@ struct BOUNDARY
 {
     int left;
     int right;
+    BOUNDARY* next = NULL;
 };
 int number[5010][30] = {0};
 BOUNDARY* boundary[250] = {NULL};
+int count_of_number[5010]; // 记录每个数组有几个数字
 
 int main()
 {
@@ -49,6 +51,7 @@ int main()
             ch = getchar();
         }
         number[groupNum][numberCnt] = atoi(tmp.c_str());
+        count_of_number[groupNum] = numberCnt + 1;
         sort_number.push_back(number[groupNum][numberCnt]);
         tmp.clear();
         groupNum++;
@@ -68,10 +71,65 @@ int main()
                 boundary[right - left]->left = left;
                 boundary[right - left]->right = right;
             }
+            else
+            {
+                BOUNDARY* tmp = boundary[right - left];
+                while (tmp->next != NULL)
+                {
+                    tmp = tmp->next;
+                }
+                tmp->next = new BOUNDARY;
+                tmp->next->left = left;
+                tmp->next->right = right;
+            }
         }
     }
 
+    for (int i = 1; i <= maxBoundary;)
+    {
+        if (boundary[i] == NULL)
+        {
+            i++;
+            continue;
+        }
 
-
+        bool appear = false;
+        for (int j = 0; j < n; j++)
+        {
+            int number_count = count_of_number[j];
+            appear = false; // 重置，为当前数组做准备
+            for (int k = 0; k < number_count; k++) // 第i个数组的第j个数字逐个判断
+            {
+                if (number[j][k] > boundary[i]->right)
+                {
+                    appear = false;
+                    break;
+                }
+                else if (number[j][k] <= boundary[i]->right && number[j][k] >= boundary[i]->left)
+                {
+                    appear = true;
+                    break; // 说明改数组中有一个数字出现在了该范围中，不用判断其他数字，判断下一个数组
+                }
+            }
+            if (appear == false) //  如果该数组中没有一个数字出现在该范围中，说明该范围不满足条件，不用判断其他数组，选取下一个范围
+            {
+                break;
+            }
+        }
+        // 所有数组都判断完毕，如果appear为true，说明该范围满足条件，输出
+        if (appear)
+        {
+            printf("%d %d" , boundary[i]->left , boundary[i]->right);
+            return 0;
+        }
+        else if (boundary[i]->next != NULL)
+        {
+            boundary[i] = boundary[i]->next;
+        }
+        else
+        {
+            i++;
+        }
+    }
     return 0;
 }
