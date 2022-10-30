1bc0b11e002d3c1b61deef5c8fde944e27cf0e05
moc <1287607885@qq.com>
2022-10-30 22:00:27 +0800

    第六周第一题

--- SixthWeek/SixthQ1.cpp
+++ SixthWeek/SixthQ1.cpp
@@ -1,3 +1,28 @@
 //
 // Created by Hleonor on 2022-10-23.
 //
+#include <bits/stdc++.h>
+using namespace std;
+
+string tower[4] =
+        { {'*'},
+          {" *\n* *\n *"},
+          {"    *\n   * *\n    *\n *     *\n* *   * *\n *     *\n    *\n   * *\n    *"},
+          {"        *\n       * *\n        *\n     *     *\n    * *   * *\n     *     *\n        *\n       * *\n        *\n *             *\n* *           * *\n *             *\n        *\n       * *\n        *\n     *     *\n    * *   * *\n     *     *\n        *\n       * *\n        *"}
+        };
+void printStar(string temp, int cnt)
+{
+    int len = temp.length();
+    string str;
+    for (int i = 1; i <= len; i++)
+    {
+        str += ' ';
+    }
+
+}
+
+int main()
+{
+
+    return 0;
+}
