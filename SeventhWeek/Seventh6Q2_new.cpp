7d98a70ee26a8a20d7b4ee68d5b49d2ed9c9aca3
moc <1287607885@qq.com>
2022-10-30 23:43:46 +0800

    Other files

---
+++ SeventhWeek/Seventh6Q2_new.cpp
@@ -0,0 +1,54 @@
+//
+// Created by Hleonor on 2022-10-30.
+//
+#include<iostream>
+using namespace std;
+
+int n, m;
+int map[3010][3010] = {0};
+int sumOfDimond = 0;
+
+void findDimond(int x)
+{
+    int num = 0;
+    int node[3010];
+    for (int i = 1; i <= n; i++)
+    {
+        if (map[x][i])
+        {
+            node[num] = i; // 记录节点x能到的点
+            num++;
+        }
+    }
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = i + 1; j < num; j++)
+        {
+             for (int k = 1; k <= n; k++)
+             {
+                 if (map[node[i]][k] && map[node[j]][k] && k != x)
+                 {
+                     sumOfDimond++;
+                 }
+             }
+        }
+    }
+}
+
+int main()
+{
+    scanf("%d %d", &n, &m);
+    int v1, v2;
+    for (int i = 1; i <= m; i++)
+    {
+        scanf("%d %d", &v1, &v2);
+        map[v1][v2] = 1;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        findDimond(i);
+    }
+    printf("%d", sumOfDimond);
+
+    return 0;
+}
