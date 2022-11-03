8fb300ab3b13073669f53f5c08611eae526207ad
moc <1287607885@qq.com>
2022-11-03 20:16:25 +0800

    第八周第四题

---
+++ EighthWeek/EighthQ4.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Hleonor on 2022-11-03.
+//
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main( )
+{
+    int n;
+    scanf("%d", &n);
+    long long ret = pow(2, n);
+    printf("%lld", ret);
+    return 0;
+}
