8ddc65bd382e4fb989659f43786ea20d56ec6fb7
moc <1287607885@qq.com>
2022-11-05 11:19:18 +0800

    第二题没处理负数的情况

--- EighthWeek/EighthQ2.cpp
+++ EighthWeek/EighthQ2.cpp
@@ -2,94 +2,28 @@
 // Created by Hleonor on 2022-11-03.
 //
 #include<bits/stdc++.h>
-#include<iostream>
-#include<algorithm>
 using namespace std;
-#define LL long long
-LL n;
-bool mk[100010];
-int p[100010], len;
-LL read() // 读入函数
+
+int main()
 {
-    LL x = 0, f = 1; char ch = getchar();
-    while (ch < '0' || ch>'9')
+    long long n;
+    scanf("%lld", &n);
+    if (n == 1)
     {
-        if (ch == '-')
-        {
-            f = -1;
-        }
-        ch = getchar();
+        printf("1");
+        exit(0);
     }
-    while (ch >= '0' && ch <= '9')
+    for (long long a = 2; a * a <= n; a++) // 至少保证了2次方
     {
-        x = x * 10 + ch - '0';
-        ch = getchar();
-    }
-    return x * f;
-}
-LL LLabs(LL a)
-{
-    return a < 0 ? -a : a;
-}
-void getp()
-{
-    for (int i = 2; i <= 100000; i++)
-    {
-        if (!mk[i])
+        for (long long b = 2; pow(a, b) <= n; b++) // 在保证了2次方的基础上往上加
         {
-            p[++len] = i;
-            for (int j = 2 * i; j <= 100000; j += i)
+            if (pow(a, b) == n) // 第一个找到的就是底数最小的，对应指数最大，直接输出指数
             {
-                mk[j] = 1;
+                printf("%lld", b);
+                exit(0);
             }
         }
     }
-}
-void work()
-{
-    int flag = (n < 0), ans = 0;
-    n = LLabs(n);
-    for (int i = 1; i <= len; i++)
-    {
-        if ((LL)p[i] * p[i] > n)
-        {
-            break;
-        }
-        if (n % p[i] != 0)
-        {
-            continue;
-        }
-        int now = 0;
-        while (n % p[i] == 0)
-        {
-            n /= p[i], now++;
-        }
-        if (!ans)
-        {
-            ans = now;
-        }
-        else
-        {
-            ans = __gcd(ans, now);
-        }
-    }
-    if (n != 1)
-    {
-        ans = 1;
-    }
-
-    if (flag)
-    {
-        while (ans % 2 == 0)
-        {
-            ans /= 2;
-        }
-    }
-    cout << ans << endl;
-}
-int main()
-{
-    getp();
-    scanf("%lld", &n);
-    work();
+    // printf("%lld", max_b);
+    return 0;
 }
